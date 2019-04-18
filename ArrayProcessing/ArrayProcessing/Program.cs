b0c68e9a2bd3df0cce6b5c430eaeceaf664c4a13
Jashandeep  Kingra <730108@student.tor.loc>
2019-04-18 13:50:35 -0400

    firstcommit

--- ArrayProcessing/ArrayProcessing/Program.cs
+++ ArrayProcessing/ArrayProcessing/Program.cs
@@ -10,6 +10,26 @@ namespace ArrayProcessing
     {
         static void Main(string[] args)
         {
+            int[] arr = { 17, 11, 9, 3, 4, 11, 6, 11, 8, 7 };
+            int arr_size = arr.Length;
+
+            printRepeating(arr, arr_size);
         }
+    
+
+    static void printRepeating(int[] arr, int size)
+    {
+        int i, j;
+        Console.WriteLine("Element which occur three times");
+        for (i = 0; i < size; i++) ;
+        {
+            for (j = 0; j < size; j++) ;
+            {
+                if (arr[i] == arr[j])
+                    Console.Write(arr[i] + " ");
+            }
+        }
+     
     }
-}
+
+
